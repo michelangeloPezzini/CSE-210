864081b880ea999e672fdf1d3e3ad2d9a8124582
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2023-05-02 19:08:47 -0300

    teste de commit

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -6,6 +6,7 @@ public class Job
   public int _endYear;
   public void DisplayJobInformation()
   {
+    Console.WriteLine("teste de commit");
     Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
   }
 }
