50f528c62ecb8fe57d8c807f85a035ec98f67326
Michelangelo Giuseppe Pezzini <lds.pezzini@gmail.com>
2023-05-16 22:49:24 -0300

    week 05 fraction practice

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+  static void Main(string[] args)
+  {
+    Fraction f1 = new Fraction();
+    Console.WriteLine(f1.GetFractionString());
+    Console.WriteLine(f1.GetDecimalValue());
+    Fraction f2 = new Fraction(10);
+    Console.WriteLine(f2.GetFractionString());
+    Console.WriteLine(f2.GetDecimalValue());
+    Fraction f3 = new Fraction(15, 5);
+    Console.WriteLine(f3.GetFractionString());
+    Console.WriteLine(f3.GetDecimalValue());
+  }
 }
